69c00e54d1778ff2e5fce26fb8cc6d753c49a605
Dim <Dim@DESKTOP-2JEO7BL>
2020-02-14 15:08:59 +0300

    Подключение и загрузка файла конфигурации Апп appsettings.json в

---
+++ Service/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.Service
+{
+    /// <summary>
+    /// Класс реализыует работу с настройками appsettings.json
+    /// </summary>
+    public  class Config
+    {
+        public static string ConnectionString { get; set; }
+        public static string CompanyName { get; set; }
+        public static string CompanyPhone { get; set; }
+        public static string CompanyPhoneShort { get; set; }
+        public static string CompanyEmail { get; set; }
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,17 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCompany.Service;
 
 namespace MyCompany
 {
     public class Startup
     {
+        //интерфейс для подключения файл конфигурации appsettings.json
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // Mетод для добавления служб в контейнер
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Подключение и загрузка файла конфигурации Апп appsettings.json в 
+            Configuration.Bind("Project", new Config());
+
             //DependencyInjection
             //Добавляем потдержку контролерров и представлений MVC
             services.AddControllersWithViews();
@@ -48,7 +56,7 @@ namespace MyCompany
             //});
 
             //указываем начальную точку(адрес) по умолчанию.(это контроле Home и экшен(метод)Index
-            //ели ни каких данных в запросе не приходит. Используются эта натсройка
+            //если ни каких данных в запросе не приходит. Используются эта настройка
             app.UseEndpoints(endpoints =>
                 {
                     endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
