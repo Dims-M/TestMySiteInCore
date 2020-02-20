c110cc4dd0ca8739dbffde4af398114bf8e73793
Dim <Dim@DESKTOP-2JEO7BL>
2020-02-20 17:40:19 +0300

    вьюха для ввода логина и пароля.

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCompany.Areas.Admin.Controllers
+{
+    //http://localhost:51302/account/login
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
